ab7aac43f795c32c499e3d2c2c6c4fefd7510e5a
a45474 <ghswkgo@gmail.com>
2020-03-09 23:15:54 +0900

    성무검도관 메인슬라이더 작동 간단하게

--- sungmu/common.js
+++ sungmu/common.js
@@ -1,14 +1,14 @@
 $('.top-bar > .inner > .right-menu-list > .cell').click(function () {
 
     $(this).toggleClass('active');
-    
+
 });
 
 $('.top-bar > .inner > .right-menu-list > .cell-right').click(function () {
 
     $('.popup-bg').addClass('active');
     $('.popup-all-menu').addClass('active');
-    
+
 });
 
 $('.popup-all-menu > .close-btn').click(function () {
@@ -21,21 +21,55 @@ $('.popup-bg').click(function () {
     $('.popup-all-menu').removeClass('active');
 });
 
-$(function() {
+$(function () {
     $.scrollify({
-        section:".page",
+        section: ".page",
         updateHash: true,
         touchScroll: true,
         setHeights: false,
-        interstitialSection:".footer",
-        before:function(i,page) {
+        interstitialSection: ".footer",
+        before: function (i, page) {
             $('html').attr('data-current-index', i);
             var ref = page[i].attr("data-section-name");
             $(".pagination .active").removeClass("active");
             $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
         },
-        afterRender:function() {
+        afterRender: function () {
             $(".pagination-box a").on("click", $.scrollify.move);
         }
     });
 });
+
+$('.slider-box > .slider > .side-btns > div').click(function () {
+    var $this = $(this);
+    var index = $this.index();
+    var $slider = $this.closest('.slider');
+    var $slides = $slider.find('> .slides');
+    var $current = $slider.find('> .slides > div.active');
+    var $post;
+
+    // console.log($slider.length);
+
+    if (index == 0) {
+        $post = $current.prev();
+    } else {
+        $post = $current.next();
+    }
+
+    if ($post.length == 0) {
+        if (index == 0) {
+            $post = $slides.find(':last-child');
+        } else {
+            $post = $slides.find(':first-child');
+        }
+    }
+
+    $current.removeClass('active');
+    $post.addClass('active');
+});
+
+function nextBtnClick () {
+    $('.slider-box > .slider > .side-btns > div:nth-of-type(2)').click();
+}
+
+setInterval(nextBtnClick, 3000);
